5145203fb34f6d7ce0c51af09a2a154038b90750
toshiya1015programmer <toshiya1015programmer@yahoo.co.jp>
2021-11-14 16:18:35 +0000

    グリッドは二部グラフという性質を使ったけど、絶対違う

--- src/typical90/072.cpp
+++ src/typical90/072.cpp
@@ -1,5 +1,7 @@
 #include <bits/stdc++.h>
+#include <atcoder/dsu>
 using namespace std;
+using namespace atcoder;
 int dx[] = {1, 0, -1, 0};
 int dy[] = {0, 1, 0, -1};
 
@@ -34,11 +36,89 @@ int main() {
     }
     if (delcnt == 0) break;
   }
-  int ans = 0;
-  for (int i = 0; i < h * w; i++) {
-    ans += can[i];
+  int groupCount;
+  {
+    dsu dsu(h * w);
+    for (int x = 0; x < h; x++) {
+      for (int y = 0; y < w; y++) {
+        if (!can[x * w + y]) continue;
+        if (x < h - 1 && can[(x + 1) * w + y]) {
+          dsu.merge(x * w + y, (x + 1) * w + y);
+        }
+        if (y < w - 1 && can[x * w + (y + 1)]) {
+          dsu.merge(x * w + y, x * w + (y + 1));
+        }
+      }
+    }
+    set<int> se;
+    for (int x = 0; x < h; x++)
+    for (int y = 0; y < w; y++) {
+      if (!can[x * w + y]) continue;
+      se.insert(dsu.leader(x * w + y));
+    }
+    groupCount = se.size();
   }
-  if (ans % 2 == 1) ans--;
+  set<pair<int, int>> splits;
+  for (int i = 0; i < h; i++) {
+    for (int j = 0; j < w; j++) {
+      if (!can[i * w + j]) continue;
+      dsu dsu(h * w);
+      can[i * w + j] = false;
+      for (int x = 0; x < h; x++) {
+        for (int y = 0; y < w; y++) {
+          if (!can[x * w + y]) continue;
+          if (x < h - 1 && can[(x + 1) * w + y]) {
+            dsu.merge(x * w + y, (x + 1) * w + y);
+          }
+          if (y < w - 1 && can[x * w + (y + 1)]) {
+            dsu.merge(x * w + y, x * w + (y + 1));
+          }
+        }
+      }
+      set<int> se;
+      for (int x = 0; x < h; x++)
+      for (int y = 0; y < w; y++) {
+        if (!can[x * w + y]) continue;
+        se.insert(dsu.leader(x * w + y));
+      }
+      if ((int) se.size() > groupCount) {
+        splits.emplace(i, j);
+      }
+      can[i * w + j] = true;
+    }
+  }
+  map<int, map<int, int>> cnts;
+  int index = 0;
+  for (int i = 0; i < h; i++) {
+    for (int j = 0; j < w; j++) {
+      if (!can[i * w + j]) continue;
+      queue<pair<int, int>> que;
+      vector<bool> seen(h * w);
+      que.emplace(i, j);
+      seen[i * w + j] = true;
+      while (que.size()) {
+        auto [x, y] = que.front();
+        que.pop();
+        cnts[index][(x + y) % 2] += 1;
+        if (splits.count(make_pair(x, y)) > 0) {
+          continue;
+        }
+        for (int k = 0; k < 4; k++) {
+          int nx = x + dx[k];
+          int ny = y + dy[k];
+          if (nx < 0 || nx >= h || ny < 0 || ny >= w) continue;
+          if (!can[nx * w + ny] || seen[nx * w + ny]) continue;
+          que.emplace(nx, ny);
+          seen[nx * w + ny] = true;
+        }
+      }
+      index += 1;
+    }
+  }
+  int ans = 0;
+  for (auto p : cnts) {
+    ans = max(ans, min(p.second[0], p.second[1]) * 2);
+  }
   if (ans == 0) ans = -1;
   cout << ans << endl;
   return 0;
