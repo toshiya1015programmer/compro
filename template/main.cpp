34cfe3defb96a9c25767a5e3fe4bf7069d48843f
toshiya1015programmer <toshiya1015programmer@yahoo.co.jp>
2021-11-15 22:46:54 +0000

    テンプレートの追加

---
+++ template/main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define popcount __builtin_popcountll
+using namespace std;
+using namespace atcoder;
+
+int main() {
+  
+  return 0;
+}
