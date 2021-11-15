9d7c0d11186f3051ded583fb699084ed15a19d62
toshiya1015programmer <toshiya1015programmer@yahoo.co.jp>
2021-11-15 13:15:03 +0000

    3進数で1減らしていく操作。ポテンシャル？

--- src/typical90/074.cpp
+++ src/typical90/074.cpp
@@ -18,53 +18,5 @@ int main() {
   string s;
   cin >> s;
   cout << solve(s) << endl;
-  // vector<string> ss = [&]() {
-  //   string abc = "abc";
-  //   vector<string> ss;
-  //   string t;
-  //   auto dfs = [&](auto dfs, int x = 0) {
-  //     if (x == n) {
-  //       ss.push_back(t);
-  //       return;
-  //     }
-  //     for (int i = 0; i < 3; i++) {
-  //       t.push_back(abc[i]);
-  //       dfs(dfs, x + 1);
-  //       t.pop_back();
-  //     }
-  //   };
-  //   dfs(dfs);
-  //   return ss;
-  // }();
-  // // simple
-  // map<string, int> dp;
-  // dp[string(n, 'a')] = 0;
-  // auto dfs = [&]() {
-  //   auto dfs = [&](auto dfs, string s) -> int {
-  //     if (dp.count(s) > 0) {
-  //       return dp[s];
-  //     }
-  //     int ret = 0;
-  //     for (int i = 0; i < n; i++) {
-  //       if (s[i] == 'a') continue;
-  //       string t = s;
-  //       t[i] -= 1;
-  //       for (int j = 0; j < i; j++) {
-  //         t[j] += 1;
-  //         if (t[j] == 'd') t[j] = 'a';
-  //       }
-  //       auto r = dfs(dfs, t) + 1;
-  //       if (ret < r) {
-  //         ret = r;
-  //       }
-  //     }
-  //     return dp[s] = ret;
-  //   };
-  //   return dfs;
-  // }();
-  // for (int i = 0; i < (int) ss.size(); i++) {
-  //   int ans = dfs(dfs, ss[i]);
-  //   cout << ss[i] << " " << ans << " " << solve(ss[i]) << '\n';
-  // }
   return 0;
 }
