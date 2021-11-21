f02494ef77d6cdca7fabb6184d5ae3489321e6d2
toshiya1015programmer <toshiya1015programmer@yahoo.co.jp>
2021-11-21 15:13:09 +0000

    repsは使わない

--- template/main.cpp
+++ template/main.cpp
@@ -1,7 +1,6 @@
 #include <bits/stdc++.h>
 #include <atcoder/all>
 #define rep(i, n) for (int i = 0; i < (int) (n); i++)
-#define reps(i, n) for (int i = 1; i <= (int) (n); i++)
 #define popcount __builtin_popcountll
 using namespace std;
 using namespace atcoder;
