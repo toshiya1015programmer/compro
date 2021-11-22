3561dbaea9e422e2629e4c07e24044ace43df0c6
toshiya1015programmer <toshiya1015programmer@yahoo.co.jp>
2021-11-22 14:10:31 +0000

    sort関数に等号付きの比較関数を渡すと無限再帰に陥る

---
+++ src/abc225/f_editorial.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+#define rep(i, n) for (int i = 0; i < (int) (n); i++)
+#define popcount __builtin_popcountll
+#define chmin(x, y) x = min(x, y)
+using namespace std;
+using namespace atcoder;
+
+int main() {
+  int n, k;
+  cin >> n >> k;
+  vector<string> s(n);
+  rep(i, n) cin >> s[i];
+  sort(s.begin(), s.end(), [&](string s, string t) {
+    // return s + t <= t + s; は RE
+    return s + t < t + s;
+  });
+  reverse(s.begin(), s.end());
+  vector dp(n + 1, vector<string>(k + 2, "~"));
+  dp[0][0] = "";
+  rep(i, n) rep(j, k + 1) {
+    if (dp[i][j] == "~") continue;
+    chmin(dp[i + 1][j], dp[i][j]);
+    chmin(dp[i + 1][j + 1], s[i] + dp[i][j]);
+  }
+  cout << dp[n][k] << endl;
+  return 0;
+}
