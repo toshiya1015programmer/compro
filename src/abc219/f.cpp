8a840b03849608c47fc0f99a68db5d51fbaff69d
toshiya1015programmer <toshiya1015programmer@yahoo.co.jp>
2021-11-24 16:04:34 +0000

    橙diffが解けた。嬉しい

---
+++ src/abc219/f.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  string s;
+  cin >> s;
+  int n = s.size();
+  long long K;
+  cin >> K;
+  set<pair<int, int>> se;
+  int x = 0, y = 0;
+  se.emplace(0 + n * 10, 0 + n * 10);
+  for (int i = 0; i < n; i++) {
+    switch (s[i]) {
+      case 'R': x++; break;
+      case 'L': x--; break;
+      case 'U': y++; break;
+      case 'D': y--; break;
+    }
+    se.emplace(x + n * 10, y + n * 10);
+  }
+  if (x == 0 && y == 0) {
+    cout << se.size() << endl;
+    return 0;
+  }
+  if (x == 0) {
+    set<pair<int, int>> newse;
+    for (auto &p : se) {
+      newse.emplace(p.second, p.first);
+    }
+    se = newse;
+    swap(x, y);
+  }
+  if (x < 0) {
+    x *= -1, y *= -1;
+  }
+  map<pair<int, int>, vector<pair<int, int>>> groups;
+  for (auto &p : se) {
+    int s = p.first / x;
+    int nx = p.first % x;
+    int ny = p.second - s * y;
+    groups[pair<int, int>(nx, ny)].emplace_back(p.first, p.second);
+  }
+  K--;
+  long long ans = 0;
+  for (auto &p : groups) {
+    auto v = p.second;
+    sort(v.begin(), v.end());
+    long long w = v[0].first;
+    int m = v.size();
+    for (int i = 0; i < m; i++) {
+      auto ne = v[i].first + K * x;
+      auto ri = i < m - 1 ? v[i + 1].first : 1001001001001001001ll;
+      if (ne < ri) {
+        ans += (ne - w) / x + 1;
+        w = ri;
+      } else {
+        continue;
+      }
+    }
+  }
+  cout << ans << endl;
+  return 0;
+}
--- template/main.cpp
+++ template/main.cpp
@@ -1,38 +1,9 @@
-#include<atcoder/all>
-#include<bits/stdc++.h>
-#define all(x) (x).begin(),(x).end()
-#define bit(x) (1ll<<(x))
-#define clz(x) __builtin_clz(x)
-#define pcnt(x) __builtin_popcountll(x)
-#define rep(i,n) for(int i=0;i<int(n);i++)
-#define repc(i,n) for(int i=0;i<=int(n);i++)
-#define reps(i,n) for(int i=1;i<=int(n);i++)
-#define rrep(i,n) for(int i=int(n)-1;i>=0;i--)
-#define rrepc(i,n) for(int i=int(n);i>=0;i--)
-#define rreps(i,n) for(int i=int(n);i>=1;i--)
-#define sz(x) int((x).size())
-using namespace atcoder;
+#include <bits/stdc++.h>
+#include <atcoder/all>
 using namespace std;
-using i64=long long;
-using f80=long double;
-using pii=pair<int,int>;
-using vi32=vector<int>;
-using vi64=vector<i64>;
-using vf80=vector<f80>;
-using vstr=vector<string>;
-template<class T>struct pqasc:priority_queue<T,vector<T>,greater<T>>{};
-template<class T>struct pqdesc:priority_queue<T,vector<T>,less<T>>{};
-template<class T>bool amax(T&a,const T&b){if(a<b){a=b;return 1;}return 0;}
-template<class T>bool amin(T&a,const T&b){if(a>b){a=b;return 1;}return 0;}
-template<class T,class S>ostream&operator<<(ostream &o,const pair<T,S>&p){o<<p.first<<","<<p.second;return o;}
-template<class T>istream&operator>>(istream&i,vector<T>&v){for(auto&x:v)i>>x;return i;}
-template<class T>ostream&operator<<(ostream &o,const vector<T>&v){rep(i,sz(v)){if(i)o<<" ";o<<v[i];}return o;}
-void solve();int main(){ios::sync_with_stdio(0);cin.tie(0);cout<<fixed<<setprecision(16);solve();return 0;}
-const int INF=1001001001;
-const i64 LINF=1001001001001001001ll;
-const int dx[]={1,0,-1,0};
-const int dy[]={0,1,0,-1};
+using namespace atcoder;
 
-void solve() {
+int main() {
   $0
+  return 0;
 }
